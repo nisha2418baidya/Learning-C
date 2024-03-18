24e9c3bf87e9053a978fd56469784bfd8b799678
Nisha Baidya <163021505+nisha2418baidya@users.noreply.github.com>
2024-03-18 14:24:31 +0530

    first cource

---
+++ codechef/cource/LBCL01_problems_CLB001.c
@@ -0,0 +1,16 @@
+// printf: This is a function in C used for printing or displaying information on the screen.
+
+// "%d": This is a format specifier. It tells printf how to interpret the data it's given. In this case, %d is used for integers (whole numbers).
+
+// 12: This is the actual data you want to print. Here, it's the number 12.
+
+// So, when you write printf("%d", 12);, it tells the computer to print the integer 12 on the screen.
+
+// Note that this line contains a (;) semicolon at the end. Semicolon is mandatory at the end of statements in C.
+#include <stdio.h>
+
+int main() {
+  
+  printf("%d", 12);
+  
+}
